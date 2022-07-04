f6bbcf96dd853675d98cb5b05a5b13e7d4f5f7a6
juansantiago <js.urquiza@gmail.com>
2022-07-04 12:20:37 -0300

    Agregué modulo crud para usuarios y el json de usuarios. Lo requerí en el controlador auth.

--- models/Users.js
+++ models/Users.js
@@ -1,20 +1,58 @@
 // MODELO CRUD DEL JSON
 
 const fs = require('fs');
+const { all } = require('../routes/products-router');
 
 const User = {
     
-    fileName: './data/users-db.json',
+    fileName: '../data/users-db.json',
 
     getData: function () {
-        return fs.readFileSync(this.fileName, 'utf-8');
+        return JSON.parse(fs.readFileSync(this.fileName, 'utf-8'));
+    },
+
+    generateId: function () {
+        let allUsers = this.findAll();
+        let lastuser = allUsers.pop();
+        if (lastuser) {
+            return lastuser.id + 1;
+        }
+        return 1;
+    },
+
+    findAll: function () {
+        return this.getData();
+    },
+
+    findByPk: function (id) {
+        let allUsers = this.findAll();
+        let userFound = allUsers.find(elemento => elemento.id == id);
+        return userFound;
+    },
+
+    findByField: function (field, text) {
+        let allUsers = this.findAll();
+        let userFound = allUsers.find(elemento => elemento[field] == text);
+        return userFound;
     },
 
     create: function (userData) {
+        let allUsers = this.findAll();
+        let newUser = {
+            id: this.generateId(),
+            ...userData
+        }
+        allUsers.push(newUser);
+        fs.writeFileSync(this.fileName, JSON.stringify(allUsers, null, ' '));
+        return true;
+    },
 
+    delete: function (id) {
+        let allUsers = this.findAll();
+        let finalUsers = allUsers.filter(elemento => elemento.id != id); // Devuelve todos los usuarios que son distintos al que se pasó por id. Todos menos ese.
+        fs.writeFileSync(this.fileName, JSON.stringify(finalUsers, null, ' '));
+        return true;
     }
 }
 
-console.log(User.getData());
-
-//00:09 min.
+module.exports = Users;
