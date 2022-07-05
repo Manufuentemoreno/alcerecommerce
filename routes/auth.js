8cf80c6e829c3c52ec25c6ed42755c2656cc8643
juansantiago <js.urquiza@gmail.com>
2022-07-04 23:00:33 -0300

    Puse enctype multipart al formulario de registro. Agregué multer en la ruta auth para que se puedan subir la imagen de perfil a la carpeta 'profile' en images.

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const router = express.Router();
+const path = require('path');
+const multer = require('multer');
 
 const authController = require('../controllers/authController');
 
-const router = express.Router();
+let multerDiskStorage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        let folder = path.join(__dirname, '../public/images/profile');
+        callback(null, folder);
+    },
+    filename: (req, file, callback) => {
+        let imageName = Date.now() + path.extname(file.originalname);
+        callback(null, imageName);
+    }
+})
+
+fileUpload = multer({ storage: multerDiskStorage});
 
 router.get('/register', authController.showRegister);
+router.post('/register', fileUpload.single('profilePhoto'), authController.register);
 router.get('/login', authController.showLogin);
-router.post('/register', authController.register);
 
 module.exports = router;
