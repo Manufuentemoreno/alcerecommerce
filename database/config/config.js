c154bcabf4b590ab92e25c59da76006dd466977b
juansantiago <js.urquiza@gmail.com>
2022-08-16 21:24:43 -0300

    Cambié a null el valor de la contraseña de la BD

--- database/config/config.js
+++ database/config/config.js
@@ -2,7 +2,7 @@ module.exports =
 {
   "development": {
     "username": "root",
-    "password": 'Importante16',
+    "password": null,
     "database": "alcer_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
