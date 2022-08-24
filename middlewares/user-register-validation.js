04b7900ec75533eb851a4cbfaaaa7f26ccfe787b
juansantiago <js.urquiza@gmail.com>
2022-08-24 13:03:52 -0300

    Completé las validaciones de backend según sprint 7. Falta solo la de la imagen.

--- middlewares/user-register-validation.js
+++ middlewares/user-register-validation.js
@@ -1,11 +1,21 @@
 const { body } = require('express-validator');
 const userRegisterValidation = [
-    body('email').notEmpty().withMessage('El campo email no puede quedar vacío.'),
-    body('password').notEmpty().withMessage('La contraseña no puede quedar vacía.'),
-    body('confirmPassword').notEmpty().withMessage('Debe confirmar la contraseña.'),
+    body('email')
+        .notEmpty().withMessage('El campo email no puede quedar vacío.').bail()
+        .isEmail().withMessage('El email debe tener un formato válido'),
+    body('password')
+        .notEmpty().withMessage('La contraseña no puede quedar vacía.').bail()
+        .isLength({min: 8}).withMessage('La contraseña debe tener al menos 8 caracteres'),
+    body('confirmPassword')
+        .notEmpty().withMessage('Debe confirmar la contraseña.').bail()
+        .isLength({min: 8}).withMessage('La contraseña debe tener al menos 8 caracteres'),
     body('category').notEmpty().withMessage('El campo categoría no puede quedar vacío.'),
-    body('name').notEmpty().withMessage('Debe completar su nombre.'),
-    body('last_name').notEmpty().withMessage('Debe completar su apellido.'),
+    body('name')
+        .notEmpty().withMessage('Debe completar su nombre.').bail()
+        .isLength({min: 2}).withMessage('El nombre debe tener al menos 2 caracteres'),
+    body('last_name')
+        .notEmpty().withMessage('Debe completar su apellido.').bail()
+        .isLength({min: 2}).withMessage('El apellido debe tener al menos 2 caracteres'),
     body('birth_date').notEmpty().withMessage('Debe completar su fecha de nacimiento.')
 ];
 
