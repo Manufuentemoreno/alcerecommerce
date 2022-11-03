00007363ea5e6bc39120e7137606c019444fb9d4
Manuel Fuente Moreno <manu-u@live.com.ar>
2022-11-03 11:55:04 -0300

    deploy in heroku

--- database/config/config.js
+++ database/config/config.js
@@ -1,24 +1,25 @@
 module.exports = 
 {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "alcer_db",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql",
+    "port": process.env.DB_PORT
   },
   "test": {
     "username": "root",
-    "password": null,
+    "password": "abc1",
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   }
 }
